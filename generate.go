07dd3962fc0f2e7dd4b79587d8a87a2adf8c44ca
Jonathan K. Stevens <jonathanstevens89@gmail.com>
2016-12-14 22:27:15 -0700

    updated package overview

--- generate.go
+++ generate.go
@@ -1,8 +1,7 @@
 /*
 A package with a struct of the table and several methods to handle common requests will be created in the $GOPATH/src/models/{table} directory. The files that are created, for a 'User' model (for example) would be:
 
-- CRUX_User.go (containing the main CRUX methods and common methods such as ReadById, ReadAll, ReadOneByQuery, ReadByQuery, and Exec)
-    - This also validates any enum/set data type with the value passed to ensure it is one of the required fields
+- CRUX_User.go (containing the main CRUX methods and common methods such as ReadById, ReadAll, ReadOneByQuery, ReadByQuery, and Exec). This also validates any enum/set data type with the value passed to ensure it is one of the required fields
 - DAO_User.go (this will hold any custom methods used to return User object(s))
 - BO_User.go (this contains methods to be called on the User object itself)
 - User_test.go to serve as a base for your unit testing
@@ -12,34 +11,32 @@ It will also generate a connection package to share connection(s) to prevent mul
 
 # implementation
 
-    go get github.com/go-sql-driver/mysql
-    go get github.com/jonathankentstevens/gostruct
+        go get github.com/go-sql-driver/mysql
+        go get github.com/jonathankentstevens/gostruct
 
 Create a generate.go file with the following contents (including your db username/password):
 
-```go
-package main
+	package main
 
-import (
-    _ "github.com/go-sql-driver/mysql"
-    "github.com/jonathankentstevens/gostruct"
-    "log"
-)
+	import (
+	    _ "github.com/go-sql-driver/mysql"
+	    "github.com/jonathankentstevens/gostruct"
+	    "log"
+	)
 
-func main() {
-    gs := new(gostruct.Gostruct)
-    gs.Username = "<db_user>"
-    gs.Password = "<db_pass>"
-    err := gs.Generate()
-    if err != nil {
-        log.Fatalln(err)
-    }
-}
-```
+	func main() {
+	    gs := new(gostruct.Gostruct)
+	    gs.Username = "<db_user>"
+	    gs.Password = "<db_pass>"
+	    err := gs.Generate()
+	    if err != nil {
+	        log.Fatalln(err)
+	    }
+	}
 
 Then, run:
 
-    go run generate.go -tables User -db main -host localhost
+        go run generate.go -tables User -db main -host localhost
 */
 package gostruct
 
